845217c50f359b930218da0c9ee8f328d1b2e484
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-11-07 18:48:22 +0300

    Reflect Generator Script

--- GObject/test/first.h
+++ GObject/test/first.h
@@ -1,14 +1,48 @@
 #ifndef FIRST_H
 #define FIRST_H
 
+#include "gobject/gobject_defs.h"
 
 struct First
 {
 public:
+	GMETHOD()
 	int b(int a);
-	int number;
+
+	GMETHOD()
+	char sum(int a);
+
+	GMETHOD()
+	int div(int a);
+	
+	GPROPERTY()
+	char number;
 };
 
+
+
+
+class Second
+{
+public:
+	GMETHOD()
+	int b(int a);
+
+	GMETHOD()
+	int sum(int a);
+
+	GMETHOD()
+	int div(int a);
+
+	GPROPERTY()
+	int number;
+
+	GPROPERTY()
+	char s;
+	
+	GPROPERTY()
+	First a;
+};
 
 
 #endif // FIRST_H
---
+++ Programs/reflect_script/reflect_generator.py
@@ -0,0 +1,161 @@
+import os;
+import json;
+import re;
+
+REFLECT_FILE_NAME = "generated_reflect"
+REFLECT_CMAKE_NAME = "greflect"
+
+class GFileAttrib:
+    def __init__(self,file_path,file_class_attribs):
+        self.file_path = file_path;
+        self.class_attribs = file_class_attribs;
+
+class GClassAttrib:
+     def __init__(self,class_struct_type,method_attribs,prop_attribs):
+        self.class_struct_type = class_struct_type;
+        self.gmethod_attribs = method_attribs;
+        self.prop_attribs = prop_attribs;
+
+class GPropertyAttrib:
+     def __init__(self,prop_name,owner_name,full_prop):
+        self.prop_name = prop_name;
+        self.owner_name = owner_name;
+        self.full_prop = full_prop;
+
+class GMethodAttrib:
+    def __init__(self,method_name,owner_name,full_method):
+        self.method_name = method_name;
+        self.owner_name = owner_name;
+        self.full_method = full_method;
+
+class ClassStructType:
+    def __init__(self,struct_type,class_struct_name,begin_index,end_index):
+        self.struct_type = struct_type;
+        self.class_struct_name = class_struct_name;
+        self.begin_index= begin_index;
+        self.end_index = end_index;
+
+
+def find_class_end(file_content, start_pos):
+    balance = 0
+    i = start_pos
+    while i < len(file_content):
+        if file_content[i] == '{':
+            balance += 1
+        elif file_content[i] == '}':
+            balance -= 1
+            if balance == 0:
+                return i + 1
+        i += 1
+    return -1
+
+def find_class_or_struct_name_and_type(file_content):
+    class_struct_pattern = r'(class|struct)\s+(\w+)\s*{'
+    class_struct_matches = re.finditer(class_struct_pattern, file_content)
+    class_struct_objs=[];
+    for class_struct_match in class_struct_matches:
+        class_struct_type = class_struct_match.group(1)
+        class_struct_name = class_struct_match.group(2)
+        class_start_pos = class_struct_match.start();
+        class_end_pos = find_class_end(file_content,class_start_pos)
+        if(class_end_pos != -1):
+            class_struct_objs.append(ClassStructType(class_struct_type,class_struct_name,class_start_pos,class_end_pos))
+
+    return class_struct_objs;
+
+def read_attribs_in_h(filepath,attribs):
+    with open(filepath,"r") as read_file:
+        file_content = read_file.read()
+       
+
+        class_struct_types = find_class_or_struct_name_and_type(file_content);
+
+        gmethod_pattern = r'GMETHOD\(\)\s*[^;]*\n\s*([^;]+\([^\)]*\))'
+        gprop_pattern = r'GPROPERTY\(\)\s*([^;]+);'
+
+        file_class_attribs = []
+        for class_struct_type in class_struct_types:
+            file_attrib_and_val = [];
+            file_prop_attrib_and_val = [];
+            matches = re.finditer(gmethod_pattern, file_content[class_struct_type.begin_index:class_struct_type.end_index])
+            for match in matches:
+                c_method_definition = match.group(1)
+                first_blank = c_method_definition.index(' ');
+                first_paranth = c_method_definition.index('(');
+                c_method_name = c_method_definition[first_blank+1:first_paranth]
+                file_attrib_and_val.append(GMethodAttrib(c_method_name,class_struct_type.class_struct_name,c_method_definition))
+            
+            matches2 = re.finditer(gprop_pattern, file_content[class_struct_type.begin_index:class_struct_type.end_index])
+            for match in matches2:
+                c_prop_definition = match.group(1)
+                first_blank = c_prop_definition.index(' ');
+                c_prop_name = c_prop_definition[first_blank+1:]
+                file_prop_attrib_and_val.append(GPropertyAttrib(c_prop_name,class_struct_type.class_struct_name,c_prop_definition))
+            file_class_attribs.append(GClassAttrib(class_struct_type,file_attrib_and_val,file_prop_attrib_and_val))
+        attribs.append(GFileAttrib(filepath,file_class_attribs));
+
+
+def iterate_files_in_dir(dir,attribs):
+    for filename in os.listdir(dir):
+        f = os.path.join(dir,filename);
+        if os.path.isfile(f):
+            if f.endswith(".h"):
+                read_attribs_in_h(f,attribs)
+        else:
+            iterate_files_in_dir(f,attribs)
+
+def add_include(file_handle,path):
+    file_handle.write(f"#include \"{path}\"\n")
+
+def add_prop_reflect_for_class(write_file,class_name,prop_name,prop_ref):
+    write_file.write(f"\tGOBJECT_DEFINE_PROPERTY(\"{prop_name}\",&{class_name}::{prop_ref})\n")
+
+
+def begin_reflect_for_class(write_file,class_name):
+    write_file.write(f"GOBJECT_ENABLE({class_name})\n")
+
+def end_reflect_for_class(write_file):
+        write_file.write("}\n")
+
+def generate_local_cmake(reflect_cpp_path,project_cmake_path):
+    if os.path.exists(f"./{REFLECT_CMAKE_NAME}.cmake"):
+        os.remove(f"./{REFLECT_CMAKE_NAME}.cmake")
+    with open(f"{project_cmake_path}/{REFLECT_CMAKE_NAME}.cmake","w") as write_file:
+        write_file.write(f"set(GREFLECT_GENERATED_CPP \"{reflect_cpp_path}\")")
+
+def generate_reflect_cpp(gobject_path,attribs,base_path):
+    if os.path.exists(f"{base_path}/{REFLECT_FILE_NAME}.cpp"):
+        os.remove(f"{base_path}/{REFLECT_FILE_NAME}.cpp")
+    with open(f"{base_path}/{REFLECT_FILE_NAME}.cpp", "w") as write_file:
+        add_include(write_file,gobject_path)
+        # First add all class refs
+        for attrib in attribs:
+            add_include(write_file,attrib.file_path)
+        write_file.write("\n\n\n")
+        for attrib in attribs:
+            file_name = attrib.file_path
+            for class_attrib in attrib.class_attribs:
+                class_struct_type = class_attrib.class_struct_type;
+                class_name = class_struct_type.class_struct_name;
+                begin_reflect_for_class(write_file,class_name)
+                for prop_attrib in class_attrib.prop_attribs:
+                    add_prop_reflect_for_class(write_file,class_name,prop_attrib.prop_name,prop_attrib.prop_name);
+
+                end_reflect_for_class(write_file)
+    return os.path.abspath(f"{base_path}/{REFLECT_FILE_NAME}.cpp")
+
+def main(): 
+    if os.path.exists("./reflect_config.json"):
+        with open("reflect_config.json", "r") as read_file:
+            data = json.load(read_file)
+            gobject_utils_path = data["gobject_utils_path"]
+            project_cmake_path = data["project_cmake_path"]
+            attribs = [];
+            for i in data["folders"]:
+                iterate_files_in_dir(i,attribs);
+            file_path = generate_reflect_cpp(gobject_utils_path,attribs,project_cmake_path)
+            generate_local_cmake(file_path,project_cmake_path)
+            
+
+if __name__=="__main__": 
+    main() 
